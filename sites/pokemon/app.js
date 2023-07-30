7f81e89e78556948e1ad30ded89d435c460bf681
Igor Nosovskyi <igor.nosovskyi@flixbus.com>
2023-07-30 23:18:44 +0200

    refactor

--- sites/pokemon/app.js
+++ sites/pokemon/app.js
@@ -1,11 +1,20 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png
-const pokemonImg = 33;
-const content = document.querySelector(".content");
+const pokemonImg = 151;
+const content = document.querySelector("#content");
+const baseUrl =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon";
+
 for (let i = 1; i <= pokemonImg; i++) {
-  let temp = document.createElement("img");
-  temp.setAttribute(
-    "src",
-    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`
-  );
-  content.insertAdjacentElement("afterend", temp);
+  const pokemonContainer = document.createElement("div");
+  pokemonContainer.classList.add("pokemon");
+  const numPokemon = document.createElement("span");
+  numPokemon.innerText = `#${i}`;
+  const pokemonImg = document.createElement("img");
+
+  pokemonImg.setAttribute("src", `${baseUrl}/${i}.png`);
+
+  pokemonContainer.appendChild(pokemonImg);
+  pokemonContainer.appendChild(numPokemon);
+
+  content.appendChild(pokemonContainer);
 }
