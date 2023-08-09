9a2da53eb4aa004666e8b6edd917997a6ac4b46d
Igor Nosovskyi <igor.nosovskyi@flixbus.com>
2023-08-09 10:36:42 +0200

    add append functionality to list

--- events/forms/app.js
+++ events/forms/app.js
@@ -1,5 +1,13 @@
 const catForm = document.querySelector("#shelterForm");
+const catInput = document.querySelector("#catInputName");
+const list = document.querySelector("#catList");
+
 catForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log("Form was submitted");
+  const catName = catInput.value;
+  console.log(`Form was submitted with input value ${catName}`);
+  const listElem = document.createElement("li");
+  listElem.innerText = catName;
+
+  list.appendChild(listElem);
 });
