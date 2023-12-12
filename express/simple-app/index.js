29c5affd8e16e387ee0eca5840bc4d8d4138c3c7
Igor Nosovskyi <igor.nosovskyi@flixbus.com>
2023-12-12 23:24:46 +0100

    small commit to have routing

--- express/simple-app/index.js
+++ express/simple-app/index.js
@@ -1,15 +1,20 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
-app.use((req, res) => {
-  res.status(201).send("<h1>Hello!</h1>");
-});
+// app.use((_req, res) => {
+//   res.status(201).send("<h1>Hello!</h1>");
+// });
 
 app.listen(3001, () => {
   console.log("Listening port 3001");
 });
 
-app.get(3001, () => {
-  console.log("Preved medved!");
+app.get("/help", (_req, resp) => {
+  resp.status(201).send("<h1>Help!</h1>");
+  console.log("There is no help here!");
+});
+
+app.get(3000, () => {
+  console.log;
 });
