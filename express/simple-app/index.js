ce683e2ff4eedc9fbae7edffb85ffd436a4d9f79
Igor Nosovskyi <igor.nosovskyi@flixbus.com>
2023-12-18 21:37:51 +0100

    nothing special using express

--- express/simple-app/index.js
+++ express/simple-app/index.js
@@ -15,6 +15,29 @@ app.get("/help", (_req, resp) => {
   console.log("There is no help here!");
 });
 
-app.get(3000, () => {
-  console.log;
+app.get("/r/:userRequest", (req, res) => {
+  console.log(req.params);
+  const { userRequest } = req.params;
+  console.log(userRequest);
+  res.status(200).send(`You have requested subredit ${userRequest}`);
+});
+
+app.get("/r/:userRequest/:id", (req, res) => {
+  console.log(req.params);
+  const { userRequest, id } = req.params;
+  console.log(userRequest);
+  res
+    .status(200)
+    .send(
+      `You have requested subredit <b>${userRequest.toUpperCase()}</b> with id: ${id}`,
+    );
+});
+
+app.get("/search", (req, res) => {
+  const { q, dogs } = req.query;
+  res.send(`<h1>Your searching for ${q} and breed ${dogs}??</h1>`);
+});
+
+app.get("*", (_req, resp) => {
+  resp.status(404).send("<h1>Invalid page!!!</h1>");
 });
