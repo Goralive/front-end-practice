eb4c087d9408bfe29628c47a75961da6445ce731
Igor Nosovskyi <igor.nosovskyi@flixbus.com>
2024-01-03 22:00:17 +0100

    more templating👻

--- express/templating/index.js
+++ express/templating/index.js
@@ -10,11 +10,21 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/random", (req, resp) => {
+app.get("/random", (_req, resp) => {
   const randomNumber = Math.floor(Math.random() * 100) + 1;
   resp.render("random", { randomNumber });
 });
 
+app.get("/r/:subreddit", (req, resp) => {
+  const { subreddit } = req.params;
+  resp.render("subreddit", { subreddit });
+});
+
 app.listen("3001", () => {
   console.log("Listening on port 3001");
+});
+
+app.get("/cats", (_req, res) => {
+  const cats = ["Lapa", "Oma", "Marvel", "Bully"];
+  res.render("cats", { cats });
 });
