8e230b20d9d024820673acc41d3c75edef1cbc69
Igor Nosovskyi <igor.nosovskyi@flixbus.com>
2024-01-08 22:29:49 +0100

    add delte

--- api/rest/first/app.js
+++ api/rest/first/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 const { v4: uuid } = require("uuid");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-const inMemoryDb = [
+let inMemoryDb = [
   {
     id: uuid(),
     username: "Todd",
@@ -61,6 +63,12 @@ app.get("/comments/:id", (req, res) => {
   res.render("comments/show", { comment });
 });
 
+app.get("/comments/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const comment = inMemoryDb.find((c) => c.id === id);
+  res.render("comments/edit", { comment });
+});
+
 app.patch("/comments/:id", (req, res) => {
   const { id } = req.params;
   const newComment = req.body.comment;
@@ -69,9 +77,16 @@ app.patch("/comments/:id", (req, res) => {
   res.redirect(`/comments/${id}`);
 });
 
+app.delete("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  inMemoryDb = inMemoryDb.filter((c) => c.id !== id);
+  res.redirect("/comments");
+});
+
 app.get("/", (_req, res) => {
   res.render("home");
 });
+
 app.get("/register", (req, res) => {
   let { name, surname } = req.query;
   res.send(`Hello ${surname} ${name}`);
